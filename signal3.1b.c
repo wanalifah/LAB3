ada8e459798acbe0d91a017c8d1fae299942c023
wanalifah <2018250836@isiswa.uitm.edu.my>
2020-10-31 12:31:22 +0000

    question 3.1

---
+++ signal3.1b.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+int main(void)
+{
+	void sigint_handler(int sig);
+	char s[200];
+	
+	if (signal(SIGINT,sigint_handler) == SIG_ERR) {
+		perror("signal");
+		exit(1);
+	}
+	
+	void sigtstp_handler(int sig);
+	char m[200];
+	
+	if (signal(SIGTSTP,sigtstp_handler) == SIG_ERR) {
+		perror("signal");
+		exit(1);
+	}
+	
+	void sigquit_handler(int sig);
+	char n[200];
+	
+	if (signal(SIGQUIT,sigint_handler) == SIG_ERR) {
+		perror("signal");
+		exit(1);
+	}
+	
+	printf("Enter a string:\n");
+	
+	if (fgets(s,200,stdin) == NULL)
+		perror("gets");
+	else
+		printf("You entered: %s\n",s);
+	
+	return(0);
+	
+}
+
+void sigint_handler(int sig)
+{
+	printf(" This is a special signal handler for SIGINT\n");
+}
+
+void sigtstp_handler(int sig)
+{
+	printf(" This is a special signal handler for SIGTSTP\n");
+}
+
+void sigquit_handler(int sig)
+{
+	printf(" This is a special signal handler for SIGQUIT\n");
+}
+
+
